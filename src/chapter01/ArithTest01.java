b658abb650cdbf6291f2cea6052e40ad45a9ce90
bit <bit@DESKTOP-2KUPKOU>
2020-05-08 21:48:19 +0900

    20200508

--- src/chapter01/ArithTest01.java
+++ src/chapter01/ArithTest01.java
@@ -48,10 +48,14 @@ public class ArithTest01 {
 		a = 1;
 		System.out.println(++a);
 		System.out.println(a);
+		// a = a + 1
+		//System.out.println(a);
 		
 		a = 1;
 		System.out.println(a++);
 		System.out.println(a);
+		//System.out.println(a);
+		// a = a + 1
 		
 		
 
