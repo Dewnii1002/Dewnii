006cb392f54fa02b7a77d5c585f18299e09383f8
bit <bit@DESKTOP-2KUPKOU>
2020-05-08 21:25:13 +0900

    20200508

--- src/chapter01/TypeTest02.java
+++ src/chapter01/TypeTest02.java
@@ -4,6 +4,25 @@ public class TypeTest02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		byte b = 25;   //암시적
+		short s = b;   //암시적
+		int i = s;     //암시적
+		long l = i;    //암시적
+		float f = i;   //암시적
+		double d = f;  //암시적
+
+		//byte b1 = 256;
+
+		System.out.println("b = " + b);
+		System.out.println("s = " + s);
+		System.out.println("i = " + i);
+		System.out.println("l = " + l);
+
+		System.out.println("f = " + f);
+		System.out.println("d = " + d);
+		//System.out.println("b1 = " + b1);
+
 
 	}
 
